91bba4aefdcea29ddccf94680a88d5015dec7a91
David Milec <milecdav@fel.cvut.cz>
2022-09-30 13:58:35 +0200

    Added new situations to test the correct function of All in

--- src/tests/dyypholdem_test.py
+++ src/tests/dyypholdem_test.py
@@ -1,27 +1,63 @@
 import os
 import sys
-sys.path.append(os.getcwd())
+from lookahead.continual_resolving import ContinualResolving
+import settings.constants as constants
 
+sys.path.append(os.getcwd())
 
 last_state = None
 last_node = None
+continual_resolving = ContinualResolving()
 
-# game_messages = ["MATCHSTATE:0:0:r300:Qs9d|", "MATCHSTATE:0:0:r300c/:Qs9d|/6d4d3c"]
-game_messages = ["MATCHSTATE:1:3::|TcAd", "MATCHSTATE:1:3:r300c/c:|TcAd/Ts8c6h", "MATCHSTATE:1:3:r300c/cc/r900:|TcAd/Ts8c6h/As"]
-# game_messages = ["MATCHSTATE:0:0:r200:Ad9h|", "MATCHSTATE:0:0:r200c/:Ad9h|/Ac9s9d", "MATCHSTATE:0:0:r200c/cc/:Ad9h|/Ac9s9d/6s"]
+situations = [
+    [
+        "MATCHSTATE:1:21::|AhAs",
+        "MATCHSTATE:1:21:c:|AhAs",
+        "MATCHSTATE:1:21:cr500:|AhAs",
+        "MATCHSTATE:1:21:cr500r1500:|AhAs",
+        "MATCHSTATE:1:21:cr500r1500c/:|AhAs/Ks2d6h",
+        "MATCHSTATE:1:21:cr500r1500c/r7500:|AhAs/Ks2d6h",
+        "MATCHSTATE:1:21:cr500r1500c/r7500c/:|AhAs/Ks2d6h/Td",
+        "MATCHSTATE:1:21:cr500r1500c/r7500c/c:|AhAs/Ks2d6h/Td",
+    ],
+    [
+        "MATCHSTATE:0:6:r350:Td9c|",
+        "MATCHSTATE:0:6:r350c/:Td9c|/JcKs9d"
+    ],
+    [
+        "MATCHSTATE:0:0:r300:Qs9d|",
+        "MATCHSTATE:0:0:r300c/:Qs9d|/6d4d3c"
+    ],
+
+    [
+        "MATCHSTATE:1:3::|TcAd",
+        "MATCHSTATE:1:3:r300c/c:|TcAd/Ts8c6h",
+        "MATCHSTATE:1:3:r300c/cc/r900:|TcAd/Ts8c6h/As"
+    ],
+    [
+        "MATCHSTATE:0:0:r200:Ad9h|",
+        "MATCHSTATE:0:0:r200c/:Ad9h|/Ac9s9d",
+        "MATCHSTATE:0:0:r200c/cc/:Ad9h|/Ac9s9d/6s"
+    ],
+]
 
 
 def replay():
-    for message in game_messages:
-        run(message)
+    for game_messages in situations:
+        for message in game_messages:
+            run(message)
 
 
 def run(msg):
     global last_state
     global last_node
+    global continual_resolving
 
     # parse the state message
-    current_state, current_node = get_state(msg)
+    current_state, current_node, winnings = get_state(msg)
+
+    if current_state is None:
+        return
 
     # do we have a new hand?
     if last_state is None or last_state.hand_number != current_state.hand_number or current_node.street < last_node.street:
@@ -55,25 +91,28 @@ def run(msg):
 
 
 def get_state(msg):
-    arguments.logger.info(f"Parsing new state: {msg}")
-    # parse the string to our state representation
-    parsed_state = protocol_to_node.parse_state(msg)
+    arguments.logger.info(f"Received ACPC dealer message: {msg.strip()}")
 
-    # figure out if we should act
+    # 2.0 parse the string to our state representation
+    parsed_state = protocol_to_node.parse_state(msg)
+    arguments.logger.debug(parsed_state)
+
+    # 3.0 figure out if we should act
     # current player to act is us
     if parsed_state.acting_player == parsed_state.player:
         # we should not act since this is an allin situations
         if parsed_state.bet1 == parsed_state.bet2 and parsed_state.bet1 == game_settings.stack:
-            arguments.logger.debug("State parsed -> not our turn -or- all in")
+            arguments.logger.debug("All in situation")
         # we should act
         else:
-            arguments.logger.debug("State parsed -> our turn >>>")
+            arguments.logger.debug("Our turn >>>")
             # create a tree node from the current state
             node = protocol_to_node.parsed_state_to_node(parsed_state)
-            return parsed_state, node
+            return parsed_state, node, 0
     # current player to act is the opponent
     else:
-        arguments.logger.debug("State parsed -> not our turn...")
+        arguments.logger.debug("Not our turn...")
+    return None, None, 0
 
 
 if __name__ == "__main__":
@@ -85,11 +124,8 @@ if __name__ == "__main__":
     import settings.game_settings as game_settings
 
     import server.protocol_to_node as protocol_to_node
-    from lookahead.continual_resolving import ContinualResolving
 
     import utils.pseudo_random as random_
-
-    continual_resolving = ContinualResolving()
 
     arguments.logger.info("Running test")
     random_.manual_seed(0)
