3c54138e4f2720d208a458e7ba373dd44f9391e3
milecdav <milecdav@fel.cvut.cz>
2023-02-23 13:37:03 +0100

    More test situations for play against slumbot

--- src/tests/dyypholdem_slumbot_test_situations.py
+++ src/tests/dyypholdem_slumbot_test_situations.py
@@ -7,28 +7,34 @@ sys.path.append(os.getcwd())
 
 last_state = None
 last_node = None
-# continual_resolving = ContinualResolving()
+continual_resolving = ContinualResolving()
 
-game_messages = [
-    {'old_action': '', 'action': 'b200', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': []},
-    {'old_action': 'b200', 'action': 'b200b600b1800', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': []},
-    {'old_action': 'b200b600b1800', 'action': 'b200b600b1800c/', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c']},
-    {'old_action': 'b200b600b1800c/', 'action': 'b200b600b1800c/kb1800', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c']},
-    {'old_action': 'b200b600b1800c/kb1800', 'action': 'b200b600b1800c/kb1800c/', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']},
-    {'old_action': 'b200b600b1800c/kb1800c/', 'action': 'b200b600b1800c/kb1800c/kb3600', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']},
-    {'old_action': 'b200b600b1800c/kb1800c/kb3600', 'action': 'b200b600b1800c/kb1800c/kb3600b15800b16400', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']}
+game_hands = [
+    [
+        {'old_action': '', 'action': 'b200', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': []},
+        {'old_action': 'b200', 'action': 'b200b600b1800', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': []},
+        {'old_action': 'b200b600b1800', 'action': 'b200b600b1800c/', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': ['As', 'Qc', '4s']},
+        {'old_action': 'b200b600b1800c/', 'action': 'b200b600b1800c/kb1800', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': ['As', 'Qc', '4s']},
+        {'old_action': 'b200b600b1800c/kb1800', 'action': 'b200b600b1800c/kb1800c/', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': ['As', 'Qc', '4s', '9c']},
+        {'old_action': 'b200b600b1800c/kb1800c/', 'action': 'b200b600b1800c/kb1800c/kb7200', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': ['As', 'Qc', '4s', '9c']},
+        {'old_action': 'b200b600b1800c/kb1800c/kb7200', 'action': 'b200b600b1800c/kb1800c/kb7200c/', 'client_pos': 0, 'hole_cards': ['Jc', 'Tc'], 'board': ['As', 'Qc', '4s', '9c', '7c']},
+    ],
+    [
+        {'old_action': '', 'action': 'b200', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': []},
+        {'old_action': 'b200', 'action': 'b200b600b1800', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': []},
+        {'old_action': 'b200b600b1800', 'action': 'b200b600b1800c/', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c']},
+        {'old_action': 'b200b600b1800c/', 'action': 'b200b600b1800c/kb1800', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c']},
+        {'old_action': 'b200b600b1800c/kb1800', 'action': 'b200b600b1800c/kb1800c/', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']},
+        {'old_action': 'b200b600b1800c/kb1800c/', 'action': 'b200b600b1800c/kb1800c/kb3600', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']},
+        {'old_action': 'b200b600b1800c/kb1800c/kb3600', 'action': 'b200b600b1800c/kb1800c/kb3600b15800b16400', 'client_pos': 0, 'hole_cards': ['5h', '5d'], 'board': ['9h', '6s', '5c', 'Ts']}
+    ]
 ]
 
 
-# game_messages = ["MATCHSTATE:0:6:r350:Td9c|", "MATCHSTATE:0:6:r350c/:Td9c|/JcKs9d"]
-# game_messages = ["MATCHSTATE:0:0:r300:Qs9d|", "MATCHSTATE:0:0:r300c/:Qs9d|/6d4d3c"]
-# game_messages = ["MATCHSTATE:1:3::|TcAd", "MATCHSTATE:1:3:r300c/c:|TcAd/Ts8c6h", "MATCHSTATE:1:3:r300c/cc/r900:|TcAd/Ts8c6h/As"]
-# game_messages = ["MATCHSTATE:0:0:r200:Ad9h|", "MATCHSTATE:0:0:r200c/:Ad9h|/Ac9s9d", "MATCHSTATE:0:0:r200c/cc/:Ad9h|/Ac9s9d/6s"]
-
-
 def replay():
-    for message in game_messages:
-        run(message)
+    for hand in game_hands:
+        for message in hand:
+            run(message)
 
 
 def run(msg):
@@ -39,14 +45,14 @@ def run(msg):
     # parse the state message
     current_state, current_node, winnings = get_state(msg)
 
-    # if msg.get('old_action') == "":
-        # continual_resolving.start_new_hand(current_state)
+    if msg.get('old_action') == "":
+        continual_resolving.start_new_hand(current_state)
 
     # do we have a new hand?
     if winnings:
         print(f"Hand ended with winnings: {winnings}")
     # use continual resolving to find a strategy and make an action in the current node
-    # advised_action: protocol_to_node.Action = continual_resolving.compute_action(current_state, current_node)
+    advised_action: protocol_to_node.Action = continual_resolving.compute_action(current_state, current_node)
 
     last_state = current_state
     last_node = current_node
@@ -79,12 +85,13 @@ if __name__ == "__main__":
     import torch
 
     import settings.arguments as arguments
-    import settings.game_settings as game_settings
 
     import server.protocol_to_node as protocol_to_node
     from server.slumbot_game import SlumbotGame
 
     import utils.pseudo_random as random_
+
+    random_.manual_seed(0)
 
     slumbot_game = SlumbotGame()
 
