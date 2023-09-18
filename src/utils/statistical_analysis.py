365c5b8be549791a42fe456f57798a0dfee17ead
milecdav <milecdav@fel.cvut.cz>
2023-09-18 09:54:45 +0200

    statistical_analysis.py changed to handle any number of methods

--- src/utils/statistical_analysis.py
+++ src/utils/statistical_analysis.py
@@ -2,6 +2,7 @@ import settings.arguments as arguments
 import utils.stats as my_stats
 
 if __name__ == '__main__':
-    folder_name = arguments.results_path + "/" + arguments.results_folder
-    interval, mean = my_stats.compute_confidence_interval(my_stats.open_all_files_and_combine_data(folder_name))
-    arguments.logger.success(f"Computed the statistics, mean: {mean} ± {interval[1] - mean}")
+    for last_folder_name in arguments.results_folder:
+        folder_name = arguments.results_path + "/" + last_folder_name
+        interval, mean = my_stats.compute_confidence_interval(my_stats.open_all_files_and_combine_data(folder_name))
+        arguments.logger.success(f"Computed the statistics for {last_folder_name}, mean: {mean} ± {interval[1] - mean}")
